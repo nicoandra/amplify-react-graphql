da842fa72c4ad2e8c5eced0f1ce641d7ec1a9e11
Nico Andrade <nic@Nicos-MacBook-Pro.local>
2023-03-05 16:00:16 -0500

    v2 commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code>, save and push to Main to redeploy.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Need to learn React?
         </a>
       </header>
     </div>
